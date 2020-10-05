d52c7e72bb1ff6b4e7025d15068bbe41b0b09854
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2020-10-05 15:47:25 +0200

    product component setup

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,23 +1,32 @@
 import React from 'react'
+import { LinkContainer } from 'react-router-bootstrap'
 import { Navbar, Nav, Container } from 'react-bootstrap'
 const Header = () => {
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
-          <Navbar.Brand href='/'>EWOKSY</Navbar.Brand>
+          <LinkContainer to='/'>
+            <Navbar.Brand>EWOKSY</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav' />
           <Nav className='ml-auto'>
-            <Nav.Link href='/cart'>
-              <i class='fas fa-socks'></i> Koszyk
-            </Nav.Link>
-            <Nav.Link href='/cart'>
-              <i class='fas fa-heart'></i> Lista życzeń
-            </Nav.Link>
-            <Nav.Link href='/login'>
-              <i class='fas fa-user-tie'></i> Zaloguj się
-            </Nav.Link>
+            <LinkContainer to='/cart'>
+              <Nav.Link>
+                <i class='fas fa-socks'></i> Koszyk
+              </Nav.Link>
+            </LinkContainer>
+            <LinkContainer to='/cart'>
+              <Nav.Link>
+                <i class='fas fa-heart'></i> Lista życzeń
+              </Nav.Link>
+            </LinkContainer>
+            <LinkContainer to='/login'>
+              <Nav.Link>
+                <i class='fas fa-user-tie'></i> Zaloguj się
+              </Nav.Link>
+            </LinkContainer>
           </Nav>
         </Container>
       </Navbar>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,73 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products'
 
-const ProductScreen = () => {
-  return <div></div>
+const ProductScreen = ({ match }) => {
+  const product = products.find((p) => p._id === match.params.id)
+  return (
+    <>
+      <Link className='btn btn-dark my-3' to='/'>
+        Powrót
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid></Image>
+        </Col>
+        <Col md={3}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h3>{product.name} </h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <h3>Cena: ${product.price} </h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              Opis produktu: {product.description}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Cena:</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Dostępność:</Col>
+                  <Col>
+                    {product.countInStock > 0 ? 'Dostępny' : 'Brak towaru'}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button
+                  className='btn-block'
+                  type='button'
+                  disabled={product.countInStock === 0}
+                >
+                  Dodaj do koszyka
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  )
 }
 
 export default ProductScreen
