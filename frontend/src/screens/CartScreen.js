8a9a4274d431fa7fa3d5b5057d52519507bfdf8c
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2020-10-26 23:47:45 +0100

    cart screen elements etc

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -31,6 +31,12 @@ const CartScreen = ({ match, location, history }) => {
     }
   }, [dispatch, productId, qty])
 
+  const removeFromCartHandler = (id) => {
+    console.log('remove')
+  }
+  const checkoutHandler = () => {
+    console.log('checkout handler')
+  }
   return (
     <Row>
       <Col md={8}>
@@ -54,15 +60,28 @@ const CartScreen = ({ match, location, history }) => {
                   <Col md={2}>
                     <Form.Control
                       as='select'
-                      value={qty}
-                      onChange={(e) => setQty(e.target.value)}
+                      value={item.qty}
+                      onChange={(e) =>
+                        dispatch(
+                          addToCart(item.product, Number(e.target.value))
+                        )
+                      }
                     >
-                      {[...Array(product.countInStock).keys()].map((x) => (
+                      {[...Array(item.countInStock).keys()].map((x) => (
                         <option key={x + 1} value={x + 1}>
                           {x + 1}
                         </option>
                       ))}
                     </Form.Control>
+                  </Col>
+                  <Col md={2}>
+                    <Button
+                      type='button'
+                      variant='dark'
+                      onClick={() => removeFromCartHandler(item.product)}
+                    >
+                      <i className='fas fa-trash'></i>
+                    </Button>
                   </Col>
                 </Row>
               </ListGroup.Item>
@@ -70,8 +89,32 @@ const CartScreen = ({ match, location, history }) => {
           </ListGroup>
         )}
       </Col>
-      <Col md={2}></Col>
-      <Col md={2}></Col>
+      <Col md={4}>
+        <Card>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h2>
+                Razem ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
+                produktów
+              </h2>
+              $
+              {cartItems
+                .reduce((acc, item) => acc + item.qty * item.price, 0)
+                .toFixed(2)}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Button
+                type='button'
+                className='btn-block'
+                disabled={cartItems.length === 0}
+                onClick={checkoutHandler}
+              >
+                Do kasy
+              </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
+      </Col>
     </Row>
   )
 }
