2c556d40cbc0a8e56d1cc1d6281fb7ea252fe957
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2020-10-27 18:35:22 +0100

    productController setup

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,33 @@
 import asyncHandler from 'express-async-handler'
 import Product from '../models/productModel.js'
 
-const getProducts = asyncHandler(async (req, res) => {})
+// @description Fetch all products
+// @route GET /api/products
+// @access Public
+
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({})
+  // find method when passed {} - empty object -> gives everything
+
+  res.json(products)
+})
+
+// @description Fetch single product
+// @route GET /api/products/:_id
+// @access Public
+
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id)
+  // this will give me (req.params.id) whichever id that is in url
+
+  //   const product = products.find((p) => p._id === req.params.id)
+  // for each product find _id that is equal to :id (from route ) (request object.params.id - the last thing in the route)
+  if (product) {
+    res.json(product)
+  } else {
+    res.status(404)
+    throw new Error('Product not found')
+  }
+})
+
+export { getProductById, getProducts }
