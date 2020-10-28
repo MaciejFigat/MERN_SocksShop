c91872520dfee40463eb9a8633c2b19cd0dc9dbc
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2020-10-28 22:59:03 +0100

    registering new user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -30,12 +30,44 @@ const authUser = asyncHandler(async (req, res) => {
   }
 })
 
+// @description Register a new user
+// @route POST /api/users
+// @access Public
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body
+
+  const userExists = await User.findOne({ email: email })
+
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+  })
+})
+
 // @description get user profile & get token
 // @route GET /api/users/profile
 // @access private
 
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.send('success')
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
 })
 
 export { authUser, getUserProfile }
