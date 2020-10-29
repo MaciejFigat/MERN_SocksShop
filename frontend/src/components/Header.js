09f5d249cf0153ef6825a10a1da918ad7d1881bf
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2020-10-29 17:57:23 +0100

    change in nav that reflects logged in state vol 2

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,11 +2,17 @@ import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 // useDispatch when I want to call and action and useSelector when I want to bring in something from state
 import { LinkContainer } from 'react-router-bootstrap'
-import { Navbar, Nav, Container } from 'react-bootstrap'
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
 
 const Header = () => {
+  const dispatch = useDispatch()
+
   const userLogin = useSelector((state) => state.userLogin)
   const { userInfo } = userLogin
+
+  const logoutHandler = () => {
+    console.log('logout')
+  }
 
   return (
     <header>
@@ -28,12 +34,22 @@ const Header = () => {
                 <i className='fas fa-heart'></i> Lista życzeń
               </Nav.Link>
             </LinkContainer>
-            {userInfo}
-            <LinkContainer to='/login'>
-              <Nav.Link>
-                <i className='fas fa-user-tie'></i> Zaloguj się
-              </Nav.Link>
-            </LinkContainer>
+            {userInfo ? (
+              <NavDropdown title={userInfo.name} id='username'>
+                <LinkContainer to={'/profile'}>
+                  <NavDropdown.Item>Profil</NavDropdown.Item>
+                </LinkContainer>
+                <NavDropdown.Item onClick={logoutHandler}>
+                  Wyloguj się
+                </NavDropdown.Item>
+              </NavDropdown>
+            ) : (
+              <LinkContainer to='/login'>
+                <Nav.Link>
+                  <i className='fas fa-user-tie'></i> Zaloguj się
+                </Nav.Link>
+              </LinkContainer>
+            )}
           </Nav>
         </Container>
       </Navbar>
