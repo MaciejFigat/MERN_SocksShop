c57de798e50b6ff24c147376c12e03be1ba53ed9
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2020-11-07 18:01:03 +0100

    orderscreen setup

---
+++ frontend/src/screens/OrderScreen.js
@@ -0,0 +1,119 @@
+import React, { useEffect } from 'react'
+import { Row, Col, ListGroup, Image, Card, Button } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Loader from '../components/Loader'
+import { Link } from 'react-router-dom'
+import Message from '../components/Message'
+import { getOrderDetails } from '../actions/orderActions'
+
+const OrderScreen = ({ match }) => {
+  const orderId = match.params.id
+
+  const dispatch = useDispatch()
+
+  const orderDetails = useSelector((state) => state.orderDetails)
+  const { order, loading, error } = orderDetails
+
+  // here I dispatch the action
+  useEffect(() => {
+    dispatch(getOrderDetails(orderId))
+  }, [])
+
+  return loading ? (
+    <Loader />
+  ) : error ? (
+    <Message variant='danger'>{error}</Message>
+  ) : (
+    <>
+      <h1>Zamówienie {order._id}</h1>
+      <Row>
+        <Col md={8}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h2> Shipping</h2>
+              <p>
+                <strong> Adres: </strong>
+                {order.shippingAddress.address}, {order.shippingAddress.city},{' '}
+                {order.shippingAddress.postalCode},{' '}
+                {order.shippingAddress.country}{' '}
+              </p>
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <h2>Metoda płatności:</h2>
+              <strong>{order.paymentMethod}</strong>
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <h2>Zamówiony towar</h2>
+              {order.orderItems.length === 0 ? (
+                <Message>Zamówienie nie zawiera żadnej pozycji </Message>
+              ) : (
+                <ListGroup variant='flush'>
+                  {order.orderItems.map((item, index) => (
+                    <ListGroup.Item key={index}>
+                      <Row>
+                        <Col md={1}>
+                          <Image
+                            src={item.image}
+                            alt={item.name}
+                            fluid
+                            rounded
+                          />
+                        </Col>
+                        <Col>
+                          <Link to={`/product/${item.product}`}>
+                            {item.name}
+                          </Link>
+                        </Col>
+                        <Col md={4}>
+                          {item.qty} x $ {item.price} = ${' '}
+                          {item.qty * item.price}
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                  ))}
+                </ListGroup>
+              )}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={4}>
+          <Card>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <h2>Podsumowanie zakupów</h2>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Przedmioty</Col>
+                  <Col>Cena {order.itemsPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Koszt wysyłki</Col>
+                  <Col>{order.shippingPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Podatek</Col>
+                  <Col>{order.taxPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Należność ogółem</Col>
+                  <Col>{order.totalPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  )
+}
+
+export default OrderScreen
