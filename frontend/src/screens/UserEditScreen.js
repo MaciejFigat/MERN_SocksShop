12cf6f7c1122266b2fa31568475e452c699f74fb
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2020-11-18 16:45:32 +0100

    User Edit screen for adminstrator vol 1, rest later

---
+++ frontend/src/screens/UserEditScreen.js
@@ -0,0 +1,84 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { Form, Button } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import FormContainer from '../components/FormContainer'
+import { getUserDetails } from '../actions/userActions'
+
+const UserEditScreen = ({ match, history }) => {
+  const userId = match.params.id
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [isAdmin, setIsAdmin] = useState(false)
+
+  const dispatch = useDispatch()
+
+  const userDetails = useSelector((state) => state.userDetails)
+
+  const { loading, error, user } = userDetails
+
+  useEffect(() => {}, [])
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+  }
+
+  return (
+    <>
+      <Link to='/admin/userList' className='btn btn-light my-3'>
+        Wróć
+      </Link>
+
+      <FormContainer>
+        <h1>Edytuj dane użytkownika</h1>
+        {loading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant='danger'>{error}</Message>
+        ) : (
+          <Form onSubmit={submitHandler}>
+            <Form.Group controlId='name'>
+              <Form.Label>Imię</Form.Label>
+              <Form.Control
+                type='name'
+                placeholder='Wpisz imię'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId='email'>
+              <Form.Label>Adres email</Form.Label>
+              <Form.Control
+                type='email'
+                placeholder='Wpisz adres email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId='isadmin'>
+              <Form.Check
+                type='checkbox'
+                label='Is Admin'
+                checked={isAdmin}
+                onChange={(e) => setIsAdmin(e.target.checked)}
+              ></Form.Check>
+            </Form.Group>
+
+            <Button type='submit' variant='primary'>
+              Edytuj
+            </Button>
+          </Form>
+        )}
+        {message && <Message variant='danger'>{message}</Message>}
+        {error && <Message variant='danger'>{error}</Message>}
+        {loading && <Loader />}
+      </FormContainer>
+    </>
+  )
+}
+
+export default UserEditScreen
